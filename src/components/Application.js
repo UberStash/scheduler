6cd92348dcef6aee28dd84819c741a73954f236e
Colin McLellan <expiredserver@gmail.com>
2020-09-02 17:07:03 -0400

    fixed problem with axios, also performed days request and passed data to state

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import axios from "axios";
 
 import "components/Application.scss";
 import DayList from"components/DayList";
@@ -54,23 +56,23 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 
 
@@ -78,7 +80,22 @@ const days = [
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    console.log('fetch axios');
   
+  axios({
+    url: `http://localhost:8001/api/days`,
+    method: 'GET'
+  })
+  .then(result => {
+    setDays(result.data)
+  })
+  
+  }, [])
+
+
   const appointmentList = appointments.map(app => {
     return <Appointment key={app.id} {...app} />
   })
